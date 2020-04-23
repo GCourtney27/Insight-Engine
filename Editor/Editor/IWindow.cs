580820bd02b4dbcfbe7bfc4790039e70cf8427f8
Garrett Courtney <gcourtney@student.neumont.edu>
2020-04-22 20:42:31 -0700

    Moved file locations

---
+++ Editor/Editor/IWindow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace Editor
+{
+    [ComVisible(true)]
+    public interface IWindow
+    {
+        void Show();
+        void Tite(string title);
+    }
+}
---
+++ Editor/Editor/Window.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Editor
+{
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.None)]
+
+    public class Window : IWindow
+    {
+        Form window = new Form();
+        public void Show()
+        {
+            window.ShowDialog();
+        }
+
+        public void Tite(string title)
+        {
+            window.Text = title;
+        }
+    }
+}
