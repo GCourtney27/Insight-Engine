2eecac0c831bcc0ef8e2f18d3ba6237c0e3026eb
Garrett Courtney <gcourtney@student.neumont.edu>
2020-05-31 00:17:16 -0700

    Added Physically Based Rendering

--- Engine/Source/Shaders/DeferredRendering/Light_Pass.pixel.hlsl
+++ Engine/Source/Shaders/DeferredRendering/Light_Pass.pixel.hlsl
@@ -18,70 +18,56 @@ float3 GammaCorrect(float3 target);
 
 float4 main(PS_INPUT_LIGHTPASS ps_in) : SV_TARGET
 {
-    float3 result = float3(0.0, 0.0, 0.0);
 	
     float3 albedoBufferSample = t_AlbedoGBuffer.Sample(s_LinearWrapSampler, ps_in.texCoords).rgb;
     float3 normalBufferSample = t_NormalGBuffer.Sample(s_LinearWrapSampler, ps_in.texCoords).rgb;
     float3 t_RoughMetAOBufferSample = t_RoughnessMetallicAOGBuffer.Sample(s_LinearWrapSampler, ps_in.texCoords).rgb;
     float3 positionBufferSample = t_PositionGBuffer.Sample(s_LinearWrapSampler, ps_in.texCoords).rgb;
     float depthBufferSample = t_DepthGBuffer.Sample(s_LinearWrapSampler, ps_in.texCoords).r;
-    
     float roughnessSample = t_RoughMetAOBufferSample.r;
     float metallicSample = t_RoughMetAOBufferSample.g;
     float aoSample = t_RoughMetAOBufferSample.b;
     
-    float z = LinearizeDepth(depthBufferSample) / cameraFarZ;
-    
     float3 normal = (normalBufferSample);
     float3 viewDirection = normalize(cameraPosition - positionBufferSample);
+    float3 F0 = float3(0.04, 0.04, 0.04);
+    float3 baseReflectivity = lerp(F0, albedoBufferSample, metallicSample);
     
+    float LO = float3(0.0, 0.0, 0.0);
     // Calculate Radiance
     //result += CalculateDirectionalLight(dirLight, normal, viewDirection, ps_in.texCoords);
     for (int i = 0; i < MAX_POINT_LIGHTS_SUPPORTED; i++)
     {
-        result += CalculatePointLight(pointLights[i], normal, positionBufferSample, viewDirection, ps_in.texCoords);
-    }
-    
-    // PBR
-    {
+        float3 lightDir = normalize(pointLights[i].position - positionBufferSample);
+        float3 halfwayDir = normalize(viewDirection + lightDir);
+        float distance = length(lightDir);
+        float attenuation = 1.0 / (distance * distance);
+        float3 radiance = pointLights[i].diffuse * attenuation;
+        
         // Cook-Torrance BRDF
-        //float NdotV = max(dot(normal, viewDirection), 0.0000001);
-    
-        //float P_NdotL = max(dot(normal, P_L), 0.0000001);
-        //float P_HdotV = max(dot(P_H, V), 0.0f);
-        //float P_NdotH = max(dot(N, P_H), 0.0f);
-    
-        //float P_D = distributionGGX(P_NdotH, roughnessSample);
-        //float P_G = geometrySmith(NdotV, P_NdotL, roughnessSample);
-        //float3 P_F = fresnelSchlick(P_HdotV, baseReflectivity);
-    
-        //float3 P_specular = P_D * P_G * P_F;
-        //P_specular /= 4.0f * NdotV * P_NdotL;
-
-        //float3 P_kD = float3(1.0f, 1.0f, 1.0f) - P_F;
-    
-        //P_kD *= 1.0f - metallicSample;
-
-        //Lo += (P_kD * albedoSample / PI + P_specular) * pointLightRadiance * P_NdotL;
-    
-        //float Dir_NdotH = max(dot(N, Dir_H), 0.0f);
-        //float Dir_HdotV = max(dot(Dir_H, V), 0.0f);
-        //float Dir_NdotL = max(dot(N, Dir_L), 0.0000001);
-
-        //float Dir_D = distributionGGX(Dir_NdotH, roughnessSample);
-        //float Dir_G = geometrySmith(NdotV, Dir_NdotL, roughnessSample);
-        //float3 Dir_F = fresnelSchlick(Dir_HdotV, baseReflectivity);
-
-        //float3 Dir_specular = Dir_D * Dir_G * Dir_F;
-        //Dir_specular /= 4.0f * NdotV * Dir_NdotL;
-
-        //float3 Dir_kD = float3(1.0f, 1.0f, 1.0f) - Dir_F;
-    
-        //Dir_kD *= 1.0f - metallicSample;
-
-        //Lo += (Dir_kD * albedoSample / PI + Dir_specular) * directionalLightRadiance * Dir_NdotL;
+        float NdotV = max(dot(normal, viewDirection), 0.0000001);
+        float NdotL = max(dot(normal, lightDir), 0.0000001);
+        float HdotV = max(dot(halfwayDir, viewDirection), 0.0);
+        float NdotH = max(dot(normal, halfwayDir), 0.0);
+        
+        float D = DistributionGGX(NdotH, roughnessSample);
+        float G = GeometrySmith(NdotV, NdotL, roughnessSample);
+        float3 F = FresnelSchlick(HdotV, baseReflectivity);
+        
+        float3 specular = D * G * F;
+        specular /= 4.0 * NdotV * NdotL;
+        
+        float3 kD = float3(1.0, 1.0, 1.0) - F;
+        kD *= 1.0 - metallicSample;
+        
+        LO += (kD * albedoBufferSample / PI + specular) * radiance * NdotL;
     }
     
+    float3 ambient = float3(0.3, 0.3, 0.3) * albedoBufferSample * aoSample;
+    float3 result = ambient * LO;
+    
+    // HDR Tonemapping
+    result = result / (result + float3(1.0, 1.0, 1.0));
     result = GammaCorrect(result);
     return float4(result, 1.0);
 }
@@ -95,7 +81,7 @@ float3 GammaCorrect(float3 target)
 float LinearizeDepth(float depth)
 {
     float z = depth * 2.0 - 1.0; // back to NDC 
-    return (2.0 * cameraNearZ * cameraFarZ) / (cameraFarZ + cameraNearZ - z * (cameraFarZ - cameraNearZ));
+    return (2.0 * cameraNearZ * cameraFarZ) / (cameraFarZ + cameraNearZ - z * (cameraFarZ - cameraNearZ)) / cameraFarZ;
 }
 
 // TODO: Fix this its amplifyingred values
